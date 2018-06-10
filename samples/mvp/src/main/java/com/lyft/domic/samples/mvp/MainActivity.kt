530b6eb8aae28466c6b106eedbe52479cab5378b
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2018-06-10 15:22:43 -0700

    Add MVP sample.

---
+++ samples/mvp/src/main/java/com/lyft/domic/samples/mvp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.lyft.domic.samples.mvp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lyft.domic.android.rendering.AndroidRenderer
+import com.lyft.domic.samples.mvp.signin.RealSignInView
+import com.lyft.domic.samples.mvp.signin.SignInPresenter
+import com.lyft.domic.samples.shared.signin.RealSignInService
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.schedulers.Schedulers.computation
+
+class MainActivity : AppCompatActivity() {
+
+    private val disposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val view = RealSignInView(findViewById(android.R.id.content), AndroidRenderer.getInstance())
+
+        disposable += Single
+                .fromCallable { SignInPresenter(view, RealSignInService()) }
+                // Showcase that you can observe Virtual DOM on non-main thread.
+                .subscribeOn(computation())
+                .subscribe { presenter -> disposable += presenter }
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
+    }
+}
